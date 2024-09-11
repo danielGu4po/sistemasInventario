230def020c0898bebf2f0bdaabb2ec807e3d771e
danielGu4po <danielrulesu4ss10@gmail.com>
2024-09-11 16:58:47 -0600

    Diseño del menú principal
    Diseño del menú de los formularios del registro de ítems en el inventario.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,59 +1,94 @@
 @extends('layouts.app')
 
+<style>
+    .buttonAdmin{
+        width: 100%;
+        height: 120px;
+        background-color: #000;
+        color: #fff;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        border-radius: 10px;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        transition: background-color 0.3s ease, transform 0.3s ease;
+        cursor: pointer;
+        font-family: Arial, Helvetica, sans-serif;
+        margin-bottom: 20px;
+    }
+
+    .buttonAdmin:hover{
+        transform: scale(1.05);
+    }
+
+    .buttonAdmin.active{
+        transform: scale(1.05);
+    }
+
+    .fasIcon{
+        font-size: 36px;
+    }
+
+    .accordion-body{
+        margin-top: 20px;
+        padding: 20px;
+        background-color: #f7f7f7;
+        border: 1px solid #ddd;
+        border-radius: 10px;
+    }
+
+    .section{
+        padding: 20px;
+        border-bottom: 1px solid #ddd;
+    }
+</style>
+
+<script>
+    function activateButton(button){
+        const buttons = document.querySelectorAll('.buttonAdmin');
+        buttons.forEach((btn)=> {
+            btn.classList.remove('active');
+        });
+        button.classList.add('active');
+    }
+</script>
+
 @section('content')
-<div class="container">
-
-    <!-- Agregar menú con imágenes debajo del dashboard -->
-    <div class="container py-5">
-        <h2 class="text-center mb-4">Selecciona una opción</h2>
+<div class="container mt-3">
+    <div class="leyenda">
+        ¿Qué quieres registrar?
+    </div>
+    <br>
+    <div class="accordion" id="accordionExample">
         <div class="row justify-content-center">
-        
-            <!-- Opción 1 -->
-            <div class="col-12 col-sm-6 col-md-4 mb-4">
-                <div class="card h-100">
-                    <a href="{{ url('/inventarioComputo') }}">
-                        <img src="/images/ordenador.png" class="card-img-top" alt="Vista 1">
-                    </a>
-                </div>
+            <div class="col-md-3">
+                <a href="{{url('/inventarioComputo')}}" class="buttonAdmin">
+                    <i class="fas fa-desktop fasIcon"></i>
+                    <div>Computó</div>
+                </a>
             </div>
-
-            <!-- Opción 2 -->
-            <div class="col-12 col-sm-6 col-md-4 mb-4">
-                <div class="card h-100">
-                    <a href="{{ url('/inventarioRedes') }}">
-                        <img src="" class="card-img-top" alt="Vista 2">
-                    </a>
-                </div>
+            <div class="col-md-3">
+                <a href="{{url('/inventarioRedes')}}" class="buttonAdmin">
+                    <i class="fas fa-network-wired fasIcon"></i>
+                    <div>Redes</div>
+                </a>
             </div>
-
-            <!-- Opción 3 -->
-            <div class="col-12 col-sm-6 col-md-4 mb-4">
-                <div class="card h-100">
-                    <a href="">
-                        <img src="" class="card-img-top" alt="Vista 3">
-                    </a>
-                </div>
+            <div class="col-md-3">
+                <a href="" class="buttonAdmin">
+                    <i class="fas fa-mouse fasIcon"></i>
+                    <div>Misceláneo</div>
+                </a>
             </div>
-
-            <!-- Opción 4 -->
-            <div class="col-12 col-sm-6 col-md-4 mb-4">
-                <div class="card h-100">
-                    <a href="">
-                        <img src="" class="card-img-top" alt="Vista 4">
-                    </a>
-                </div>
+            <div class="col-md-3">
+                <a href="" class="buttonAdmin">
+                    <i class="fas fa-phone-volume fasIcon"></i>
+                    <div>Telefonía</div>
+                </a>
             </div>
-
-            <!-- Opción 5 -->
-            <div class="col-12 col-sm-6 col-md-4 mb-4">
-                <div class="card h-100">
-                    <a href="/vista5">
-                        <img src="" class="card-img-top" alt="Vista 5">
-                    </a>
-                </div>
-            </div>
-
         </div>
     </div>
 </div>
+
 @endsection
